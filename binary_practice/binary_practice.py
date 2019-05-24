3b3e828793b249a5c49a4b21eeb51b3384697f4c
Anthony Slavin <ehmofficial@gmail.com>
2019-05-25 00:04:45 +0300

    Remove .exe, add binary-to-decimal mode

--- binary_practice/binary_practice.py
+++ binary_practice/binary_practice.py
@@ -1,6 +1,12 @@
 from random import randint
+import os
 
-def convertNum(inp):
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def bin_to_num(inp):
     inpList = list(inp)
     decTotal = 0
     i = len(inpList) - 1
@@ -11,8 +17,37 @@ def convertNum(inp):
         
     return decTotal
 
+
+def num_to_bin(n):
+    tmp = []
+    while n:
+        n, i = divmod(n, 2)
+        tmp.append(str(i))
+    return "".join(tmp[::-1] or "0")
+
+
+def btd():
+    cls()
+    print('Convert to decimal! Type \'exit\' to exit or \'dtb\' for decimal-to-binary mode!')
+    while True:
+        randValue = num_to_bin(randint(1, 255))
+        print(randValue)
+        inp = input() 
+        
+        if str(inp) == 'exit':
+            break
+        elif str(inp) == 'dtb':
+            main()
+            break
+        elif randValue == num_to_bin(int(inp)):
+            print('Correct!')
+        elif randValue != num_to_bin(int(inp)):
+            print('Incorrect! Correct answer:', bin_to_num(randValue))
+
+
 def main():
-    print('Convert to binary! Type \'exit\' to exit.')
+    cls()
+    print('Convert to binary! Type \'exit\' to exit or \'btd\' for binary-to-decimal mode!')
     while True:
         randValue = randint(1, 255)
         print(randValue)
@@ -20,10 +55,14 @@ def main():
         
         if str(inp) == 'exit':
             break
-        elif randValue == convertNum(inp):
+        elif str(inp) == 'btd':
+            btd()
+            break
+        elif randValue == bin_to_num(inp):
             print('Correct!')
-        elif randValue != convertNum(inp):
-            print('Incorrect!')
+        elif randValue != bin_to_num(inp):
+            print('Incorrect! Correct answer:', num_to_bin(randValue))
+
 
 if __name__ == '__main__':
     main()
